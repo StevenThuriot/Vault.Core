c81b5b0eab8005cfa3858caafc6f5fca5eb336fc
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-09-02 19:53:58 +0200

    Let's just start writing some unit tests

---
+++ Vault.Core.Tests/VaultTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace Vault.Core.Tests
+{
+    [TestClass]
+    public class VaultTests
+    {
+        const string originalValue = "This is a sentence! :)";
+
+        static byte[] _value;
+        static byte[] _password;
+
+        [ClassInitialize]
+        public static void InitVaultTests(TestContext context)
+        {
+            _value = Encoding.Unicode.GetBytes(originalValue);
+            _password = Encoding.Unicode.GetBytes("This is a password!");
+        }
+
+
+        [TestMethod]
+        public void CanEncrypt()
+        {
+            var result = Security.Encrypt(_value, _password);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length != 0);
+        }
+
+        [TestMethod]
+        public void EncryptedValuesCanBeDecrypted()
+        {
+            var result = Security.Encrypt(_value, _password);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length != 0);
+
+            result = Security.Decrypt(result, _password);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length != 0);
+            Assert.AreEqual(_value.Length, result.Length);
+            CollectionAssert.AreEqual(_value, result);
+        }
+
+        [TestMethod]
+        public void EncryptedValuesCanBeDecryptedAsString()
+        {
+            var result = Security.Encrypt(_value, _password);
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length != 0);
+
+            var stringResult = Security.DecryptString(result, _password);
+
+            Assert.IsNotNull(stringResult);
+            Assert.IsTrue(stringResult.Length != 0);
+            Assert.AreEqual(originalValue, stringResult);
+        }
+    }
+}
