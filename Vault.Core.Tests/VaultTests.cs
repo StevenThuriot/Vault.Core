4651db1efe4d79a88496499f308ba4b75559889f
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-09-21 21:22:17 +0200

    Single Key Can Be Decrypted From A File Using An Index File While The Result Is Encrypted

--- Vault.Core.Tests/VaultTests.cs
+++ Vault.Core.Tests/VaultTests.cs
@@ -313,5 +313,34 @@ namespace Vault.Core.Tests
             Assert.AreEqual(originalValue2.Length, decrypted.Length);
             Assert.AreEqual(originalValue2, decrypted.ToUnsecureString());
         }
+
+        [TestMethod]
+        public void SingleKeyCanBeDecryptedFromAFileUsingAnIndexFileWhileTheResultIsEncrypted()
+        {
+            const string key = "another Key";
+            var dictionary = new Dictionary<string, SecureString>
+            {
+                {  "key", originalValue.Secure() },
+                { key, originalValue2.Secure() }
+            };
+
+
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "CanEncryptToAFile.enc");
+            File.Delete(path);
+
+            Assert.IsFalse(File.Exists(path));
+
+            Security.EncryptFile(dictionary, path, _password, EncryptionOptions.Offsets | EncryptionOptions.Result);
+
+            var file = new FileInfo(path);
+            Assert.IsTrue(file.Exists);
+            Assert.AreNotEqual(0, file.Length);
+
+            var decrypted = Security.DecryptFile(path, key, _password, options: EncryptionOptions.Offsets | EncryptionOptions.Result);
+
+            Assert.IsNotNull(decrypted);
+            Assert.AreEqual(originalValue2.Length, decrypted.Length);
+            Assert.AreEqual(originalValue2, decrypted.ToUnsecureString());
+        }
     }
 }
