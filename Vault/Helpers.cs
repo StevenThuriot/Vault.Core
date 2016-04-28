d375beaba96f95ce8131c4235544a574f8001de0
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2016-04-28 18:36:43 +0200

    scope helper

--- Vault/Helpers.cs
+++ Vault/Helpers.cs
@@ -26,5 +26,28 @@ namespace Vault.Core
             fixed (char* v = value)
                 return new SecureString(v, value.Length);
         }
+
+
+
+
+
+
+        public static IDisposable Scoped<T>(this T[] bytes) => new ScopeHelper<T>(bytes);
+
+        class ScopeHelper<T> : IDisposable
+        {
+            T[] _bytes;
+
+            public ScopeHelper(T[] bytes)
+            {
+                _bytes = bytes;
+            }
+
+            public void Dispose()
+            {
+                Array.Clear(_bytes, 0, _bytes.Length);
+                _bytes = null;
+            }
+        }
     }
 }
